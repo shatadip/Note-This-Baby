3738f3cc72709bdb0f4ff20847257b504c28b6d7
Shatadip Majumder <shatadip.majumder@gmail.com>
2024-03-24 21:25:15 +0530

    Added Quill as RTE. WIP.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import NTBComponent from './components/NTBComponent'
 import './App.css'
 
 function App() {
-
   return (
     <>
+
       <NTBComponent />
+
     </>
+
   )
+
 }
 
 export default App
--- src/components/NTBComponent.tsx
+++ src/components/NTBComponent.tsx
@@ -1,8 +1,22 @@
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'; // import styles
 
 const NTBComponent = () => {
-  return (
-    <div>NTBComponent</div>
-  )
-}
+ const modules = {
+    toolbar: [
+      [{ 'header': [1, 2, false] }],
+      ['bold', 'italic', 'underline','strike', 'blockquote'],
+      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+      ['link', 'image'],
+      ['clean']
+    ],
+ };
+
+ return (
+    <ReactQuill
+      modules={modules}
+    />
+ );
+};
 
 export default NTBComponent
